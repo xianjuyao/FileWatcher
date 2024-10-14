67c51d9e421c538634bbf124c3df54a758f3efd0
xianjuyao <2214452605@qq.com>
2024-10-14 16:36:45 +0800

    deleted usercontrol

--- FileWatcher/Views/MainWindow.xaml.cs
+++ FileWatcher/Views/MainWindow.xaml.cs
@@ -7,15 +7,6 @@ namespace FileWatcher
     /// </summary>
     public partial class MainWindow
     {
-        public static readonly DependencyProperty MyHeightProperty =
-            DependencyProperty.Register("MyHeight", typeof(string), typeof(MainWindow),
-                new PropertyMetadata("test"));
-
-        public string MyHeight
-        {
-            get { return (string)GetValue(MyHeightProperty); }
-            set { SetValue(MyHeightProperty, value); }
-        }
         public MainWindow()
         {
             InitializeComponent();
